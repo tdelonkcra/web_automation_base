d97fc3cc901a43d47a4eeb0e2342f0edc708d9b0
tdelonkcra <tdelonkcra@greenbits.com>
2019-08-15 14:31:28 -0700

    changed search to dogs added im feeling lucky

--- features/google/support/pages/home_page.rb
+++ features/google/support/pages/home_page.rb
@@ -3,4 +3,5 @@ class HomePage < SitePrism::Page
 
   element :search_bar, '[title="Search"]'
   element :search_button, '[aria-label="Google Search"]'
+  element :im_feeling_lucky_button, '[aria-label="I\'m Feeling Lucky"]'
 end
--- features/google/support/step_definitions/search_steps.rb
+++ features/google/support/step_definitions/search_steps.rb
@@ -2,7 +2,7 @@ Given("I visit Google's homepage") do
   @app.home_page.load
 end
 
-When('I input {string} into the search Bar') do |string|
+When('I input {string} into the search bar') do |string|
   @app.home_page.search_bar.set string
 end
 
@@ -10,9 +10,14 @@ When('I tap Google Search') do
   @app.home_page.search_button.click
 end
 
+When("I tap I'm Feeling Lucky") do
+  @app.home_page.im_feeling_lucky_button.click
+end
+
 Then("Google's homepage loads") do
   expect(@app.home_page).to have_search_bar
   expect(@app.home_page).to have_search_button
+  expect(@app.home_page).to have_im_feeling_lucky_button
 end
 
 Then('Search results for {string} load') do |string|
@@ -20,4 +25,8 @@ Then('Search results for {string} load') do |string|
   expect(@app.search_results_page).to have_actual_results
   expect(@app.search_results_page).to have_search_bar
   expect(@app.search_results_page.search_bar.value).to eq string
+end
+
+Then('I no longer see the Google Home Page') do
+  expect(@app.home_page).to have_no_im_feeling_lucky_button
 end
